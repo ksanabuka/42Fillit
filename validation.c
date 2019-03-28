12214e6e4b9d74ef819964921f54de6e6007170d
Oksana Buksha <oksanka@macbook-oksana.attlocal.net>
2019-03-28 08:22:41 -0700

    done

--- validation.c
+++ validation.c
@@ -1,4 +1,42 @@
 #include <stdio.h>
+#include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+#include "libft/libft.h"
+
+
+void deletenl(char *str)
+{
+	char * s;
+
+	s = str;
+
+	while (*s)
+	{
+		if (*s == '\n' && *(s + 1))
+		{
+			ft_memmove(s, s + 1, ft_strlen(s));
+			s++;
+		}
+		if (*s == '\n' && !(*(s + 1)))
+		{
+			*s = '\0';
+			return ;
+		}
+		s++;
+	}
+
+	return ;
+}
+
+// int main (void)
+// {
+// 	char  str [25] = "...#\n...#\n...#\n...#\n";
+
+// 	deletenl(str);
+// 	printf("%s\n", str);
+
+// }
 
 int validateFigureByConnections(char *s)
 {
@@ -7,13 +45,13 @@ int validateFigureByConnections(char *s)
 
 	while (s[i])
 	{
-		if ((i - 1 >= 0) && s[i-1] == '#' && s[i] == '#')
+		if ((i >= 1) && s[i-1] == '#' && s[i] == '#')
 			count++;
-		if ((i - 4 >= 0) && s[i-4] == '#' && s[i] == '#')
+		if ((i >= 4) && s[i-4] == '#' && s[i] == '#')
 			count++;
-		if ((i + 1 <= 15) && s[i+1] == '#' && s[i] == '#')
+		if ((i <= 14) && s[i+1] == '#' && s[i] == '#')
 			count++;
-		if ((i + 4 <= 15) && s[i+4] == '#' && s[i] == '#')
+		if ((i <= 11) && s[i+4] == '#' && s[i] == '#')
 			count++;
 		i++;
 	}
@@ -27,20 +65,37 @@ int validate_figure_by_chars(char *s)
 {
 	//check for that string consists only of '.' '#' && has exactly 4 '#'s. 
 	int countHash = 0;
+	int countDots = 0;
+	int countNl = 0;
+
 	int i = 0;
+
+	if (s[4] != '\n' || s[9] != '\n' || s[14] != '\n' || s[19] != '\n')
+		return (0);
+
 	while (s[i])
 	{
-		if ((s[i] != '.' && s[i] != '#' )|| countHash > 4)
-			return (0);
-		else if (s[i] == '#')
+
+		if (s[i] == '#')
 		{
 			countHash++;
 			i++;
 		}
-		else 
+		else if (s[i] == '.')
+		{
+			countDots++;
 			i++;
+		}
+		else if (s[i] == '\n')
+		{
+			countNl++;
+			i++;
+		}
+		else
+			return 0;
 	}
-	if (countHash != 4)
+
+	if (countHash != 4 || countDots != 12 || countNl != 4)
 		return (0);
 	else 
 		return (1);
@@ -69,12 +124,78 @@ int validate_figure_by_chars(char *s)
 // 	return 0;
 // }
 
-// one figure
-// 	4 x (read 4 char (. || #) \n)
-// 	#  == 4
+int readFile(char * av)
+{
+	int i = 0;
+	int flag = 0;
 
-// \n (not EOF)
-// figure++;
+	int fd;
+	//int status = 0;
+	int readsize;
+	char buffer[21];
+	ft_bzero(buffer, 21);
+
+
+	fd = open(av, O_RDONLY);
+
+	while (i <= 27)
+	{
+		if (i == 27)
+			return -1;
+
+		readsize = read(fd, buffer, 20);
+		printf("before deletion 	i = %d\n%s\n", i, buffer);
+
+		if (readsize != 20 || !validate_figure_by_chars(buffer))
+		{			
+			return -1;
+		}
+		deletenl(buffer);
+		printf("after deletion 	i = %d\n%s\n", i, buffer);
+
+		if (!validateFigureByConnections(buffer))
+			
+		{			
+			return -1;
+		}
+
+
+		//add_figure(t_list, buffer);
+		buffer[0] = '\0';
+
+		readsize = read(fd, buffer, 1);
+		
+		if (buffer[0] != '\n' && readsize != 1)
+		{			
+			return -1;
+		}
+
+		i++;
+	}
+	return 1;
+}
+
+
+int main(int ac, char ** av)
+{
+	if (ac != 2)
+		return -5; // show usage
+
+	int status;
+
+	status = readFile(av[1]);
+	
+	if (status != 1)
+		printf("Not valid\n");
+	else 
+		printf("Valid\n");
+
+	return 0;
+}
+
+
+
+
 
 
 
