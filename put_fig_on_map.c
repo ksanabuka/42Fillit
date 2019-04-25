f15e2cb6e368212605c3a8cd8bdc44784962442a
ksanabuka <obuksha@hotmail.com>
2019-04-25 14:51:29 -0700

    another try

--- put_fig_on_map.c
+++ put_fig_on_map.c
@@ -56,38 +56,6 @@ int		can_put_fig_on_map(char **map, int r_0,  int c_0, t_tetr *figure, t_tetr **
 		return (0);
 }
 
-int		can_put_fig_on_map(char **map, int r_0,  int c_0, t_tetr *figure, t_tetr **head)
-{
-	int		j;
-	int		length;
-
-	j = 0;
-	length = (*head)->curmap_length - 1;
-	if ((r_0 + figure->arr[1] <= length) && (r_0 + figure->arr[1] >= 0) && (c_0 + figure->arr[0] <= length) && (c_0 + figure->arr[0] >= 0))
-		j++;
-	if ((r_0 + figure->arr[3] <= length) && (r_0 + figure->arr[3] >= 0) && (c_0 + figure->arr[2] <= length) && (c_0 + figure->arr[2] >= 0))
-		j++;
-	if ((r_0 + figure->arr[5] <= length) && (r_0 + figure->arr[5] >= 0) && (c_0 + figure->arr[4] <= length) && (c_0 + figure->arr[4] >= 0))
-		j++;
-	if ((r_0 + figure->arr[7] <= length) && (r_0 + figure->arr[7] >= 0) && (c_0 + figure->arr[6] <= length) && (c_0 + figure->arr[6] >= 0))
-		j++;
-	if (j == 4)
-	{
-		if (map[r_0 + figure->arr[1]][c_0 + figure->arr[0]] == '.')
-			j++;
-		if (map[r_0 + figure->arr[3]][c_0 + figure->arr[2]] == '.')
-			j++;
-		if (map[r_0 + figure->arr[5]][c_0 + figure->arr[4]] == '.')
-			j++;
-		if (map[r_0 + figure->arr[7]][c_0 + figure->arr[6]] == '.')
-			j++;
-	}
-	if (j == 8)
-		return (1);
-	else
-		return (0);
-}
-
 void 	put_fig_on_map(char **map, int r_0, int c_0, t_tetr *figure)
 {
 	map[r_0][c_0] = figure->c;
