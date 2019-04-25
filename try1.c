1d6ab52fa7a960c42d167bd98b3f663c7068aeeb
ksanabuka <obuksha@hotmail.com>
2019-04-25 11:05:32 -0700

    suggested restructure in fillit.h

--- try1.c
+++ try1.c
@@ -83,49 +83,54 @@ char **find_best_solution(t_tetr **head)
 		{
 			return map;
 		}
-
 		(*head)->curmap_length++;
 	}
 	return NULL;
 }
 
+int fig_validation(int i, int readsize, char *buffer)
+{
+	if (!validate_figure_by_chars(buffer) || readsize != 20)
+		return 0;		
+	delete_nl(buffer);
+	if (!validate_figure_by_connections(buffer) || i + 1 == 27)	
+		return 0;
+	return 1; 		
+}
+
+int set_qtyFig_MinSq(int i, t_tetr **head)
+{
+	(*head)->qty_fig = i;
+	(*head)->curmap_length = find_min_sq_width((*head)->qty_fig);
+	return 5;
+}
+
 int readFile(char * av, t_tetr **head)
 {
-	int i = 0;
+	int i;
 	int fd;
 	int readsize;
 	char buffer[21];
+	
+	i = 0;
 	ft_bzero(buffer, 21);
-
 	fd = open(av, O_RDONLY);
-
 	while (i <= 27)
 	{
 		readsize = read(fd, buffer, 20);
-		if (!validate_figure_by_chars(buffer) || readsize != 20)
-			return -1;		
-		delete_nl(buffer);
-		if (!validate_figure_by_connections(buffer))	
-			return -1;		
+		if (!fig_validation(i, readsize, buffer))
+			return -1; 
 		add_tetrimonos(buffer, head, i);
 		i++;
-		if (i == 27)
-			return -1;		
 		buffer[0] = '\0';
 		readsize = read(fd, buffer, 1);		
 		if (readsize == 0)
-		{	
-			(*head)->qty_fig = i;
-			(*head)->curmap_length = find_min_sq_width((*head)->qty_fig);
-			return 5;
-		}
+			return (set_qtyFig_MinSq(i, head));
 		else if (buffer[0] != '\n' || readsize < 0)
 			return -1;		
 	}
 	return 5;
 }
-
-#include <fcntl.h>
 	
 int main (int ac, char ** av)
 {
@@ -140,19 +145,14 @@ int main (int ac, char ** av)
 	if (status == -1)
 	{
 		write(1, "error\n", 6);
-		cleanup(&head);
-		system("leaks a.out");
-	
+		cleanup(&head);	
 		exit(0);
 	}
 	char ** map = find_best_solution(&head);
 
 	displayMap(map, &head);
 	head->curmap_length++; 
-	destroy_map(map, &head);
-
-   system("leaks a.out");
-	
+	destroy_map(map, &head);	
 	return 0;
 }
 
